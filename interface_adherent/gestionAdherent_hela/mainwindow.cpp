bd71c3048854c3f834382f6f1c2685cb2db9d96b
hela <hela.hedoui@esprit.tn>
2025-09-29 11:35:20 +0100

    images.qrc

---
+++ interface_adherent/gestionAdherent_hela/mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+/*
+ adherent index 2
+atelier index 4
+equipement index 5
+formateur index 6/3
+employe index 1
+index 0 dashboard*/
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+     //ui->stackedWidget->setCurrentIndex(2);
+    connect(ui->menuList, &QListWidget::currentRowChanged,
+            ui->stackedWidget, &QStackedWidget::setCurrentIndex);
+    ui->menuList->setCurrentRow(0);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
